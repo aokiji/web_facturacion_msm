595aa4356d2e77a176c5eeefed0e39e72a40f555
Nicolas De los Santos <ndls05@gmail.com>
2016-02-08 22:13:33 +0100

    Corregido para que el menu funcione con turbolinks

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
+function initialize_menu(){
    $(".menu01").click(function(){
        if(false == $(this).next().is(":visible")) {
            $(".menu02").slideUp(300);
        }
        $(this).next().slideToggle("normal");
    });     
-});
+}
+$(document).ready(initialize_menu);
+$(document).on('page:load', initialize_menu);
