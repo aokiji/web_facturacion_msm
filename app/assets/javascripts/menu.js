86292240a4d80b1f540ce6d2b02287cb1e2ecdaa
Nicolas De los Santos <ndls05@gmail.com>
2016-02-10 23:38:22 +0100

    Hacemos que el menu se despliegue con la parte activa

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -4,7 +4,8 @@ function initialize_menu(){
            $(".menu02").slideUp(300);
        }
        $(this).next().slideToggle("normal");
-   });     
+   });
+   $(".menu01.active").click();
 }
 $(document).ready(initialize_menu);
 $(document).on('page:load', initialize_menu);
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def nav_button_for(controller, name, html_options = nil)
+        html_options[:class] ||= ""
+        classes = ['menu01']
+        if controller.to_s == params[:controller] then
+            classes += ['active']
+        end
+        html_options[:class] = (html_options[:class].split + classes).uniq.join(' ')
+
+        content_tag('a', name, html_options)
+    end
 end
